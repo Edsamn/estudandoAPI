296b1315bb817ddc49b718c4303745c1d245adea
Edsamn <eduardomsribas@gmail.comm>
2024-03-15 21:51:23 -0300

    refatoração de funções, lógica para mostrar mais resultados de pesquisa

--- scripts.js
+++ scripts.js
@@ -1,23 +1,32 @@
 const characterContainer = document.querySelector(".contentList");
-let initialPage = 1;
 const pageNumber = document.getElementById("pageNumber");
+const prevButton = document.getElementById("previousPage");
+const nameValue = document.getElementById("name").value;
+const searchButton = document.getElementById("searchButton");
+const header = document.getElementById("header");
+let initialPage = 1;
+let pageSelect = 1;
+
+const renderOnDisplay = (character) => {
+  const userCard = document.createElement("div");
+  userCard.classList.add("userCard");
+
+  userCard.innerHTML = `
+  <img src='${character.image}' alt=''>
+  <p> ${character.name} </p>
+  <p> ${character.status} </p>
+  <p> ${character.species} </p>
+  `;
+  characterContainer.appendChild(userCard);
+};
 
 async function getCharacters() {
   try {
-    const response = await api.get("/character");
+    const response = await api.get(`/character?page=${initialPage}`);
     const characters = response.data.results;
 
     characters.forEach((character) => {
-      const userCard = document.createElement("div");
-      userCard.classList.add("userCard");
-
-      userCard.innerHTML = `
-      <img src='${character.image}' alt=''>
-      <p> ${character.name} </p>
-      <p> ${character.status} </p>
-      <p> ${character.species} </p>
-      `;
-      characterContainer.appendChild(userCard);
+      renderOnDisplay(character);
       pageNumber.textContent = `Página atual: ${initialPage}`;
     });
   } catch (error) {
@@ -27,84 +36,46 @@ async function getCharacters() {
 getCharacters();
 
 async function searchCharacterByName() {
-  const nameValue = document.getElementById("name").value;
   try {
-    const response = await api.get(`/character?name=${nameValue}`);
+    if (initialPage > 1) {
+      initialPage = 1;
+    }
+    const response = await api.get(`/character?name=${nameValue}&page=${pageSelect}`);
     const characters = response.data.results;
+    const totalPages = response.data.info.pages;
 
     characterContainer.innerHTML = "";
 
     const filteredCharacters = characters.filter((character) => character.name.includes(`${nameValue}`));
-    filteredCharacters.forEach((character) => {
-      const userCard = document.createElement("div");
-      userCard.classList.add("userCard");
-
-      userCard.innerHTML = `
-      <img src='${character.image}' alt=''>
-      <p> ${character.name} </p>
-      <p> ${character.status} </p>
-      <p> ${character.species} </p>
-      `;
-      characterContainer.appendChild(userCard);
-      pageNumber.textContent = `Página atual: ${initialPage}`;
+    filteredCharacters.forEach((filteredCharacter) => {
+      renderOnDisplay(filteredCharacter);
     });
+    pageNumber.textContent = `Total de páginas da pesquisa: ${totalPages}`;
+    if (pageSelect === totalPages) {
+      searchButton.disabled;
+      pageNumber.textContent = `Fim da pesquisa.`;
+    } else {
+      pageSelect++;
+    }
+    header.textContent = "Para visualizar mais resultados da pesquisa, continue usando o botão com o campo preenchido";
   } catch (error) {
     console.log(error);
   }
 }
 
-async function nextPage() {
-  try {
-    if (initialPage) {
-      initialPage += 1;
-    }
-    const response = await api.get(`/character?page=${initialPage}`);
-    const characters = response.data.results;
+function nextPage() {
+  initialPage += 1;
 
-    characterContainer.innerHTML = "";
+  characterContainer.innerHTML = "";
 
-    characters.forEach((character) => {
-      const userCard = document.createElement("div");
-      userCard.classList.add("userCard");
-
-      userCard.innerHTML = `
-        <img src='${character.image}' alt=''>
-        <p> ${character.name} </p>
-        <p> ${character.status} </p>
-        <p> ${character.species} </p>
-        `;
-      characterContainer.appendChild(userCard);
-      pageNumber.textContent = `Página atual: ${initialPage}`;
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  getCharacters();
 }
 
-async function previousPage() {
-  try {
-    if (initialPage > 1) {
-      initialPage -= 1;
-    }
-    const response = await api.get(`/character?page=${initialPage}`);
-    const characters = response.data.results;
-
-    characterContainer.innerHTML = "";
-
-    characters.forEach((character) => {
-      const userCard = document.createElement("div");
-      userCard.classList.add("userCard");
-
-      userCard.innerHTML = `
-        <img src='${character.image}' alt=''>
-        <p> ${character.name} </p>
-        <p> ${character.status} </p>
-        <p> ${character.species} </p>
-        `;
-      characterContainer.appendChild(userCard);
-      pageNumber.textContent = `Página atual: ${initialPage}`;
-    });
-  } catch (error) {
-    console.log(error);
+function previousPage() {
+  if (initialPage > 1) {
+    initialPage -= 1;
   }
+  characterContainer.innerHTML = "";
+
+  getCharacters();
 }
